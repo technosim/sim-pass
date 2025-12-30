85fb16b9d838ad810a0d3afef3da7ee9fc1075ea
technosim <simon-94@live.com>
2025-12-31 07:32:11 +1100

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# sim-pass
-Simple PowerShell script with setup to generate user friendly passwords in CLI
+# simpass - PowerShell Profile
+
+`simpass` is a lightweight password generator for Windows PowerShell.
+
+It generates secure but user friendly passwords that are:
+- Easy to read
+- Easy to spell
+- Easy to communicate verbally
+- Automatic copy to clipboard
+
+Example output:
+
+```
+Cosmic-Kangaroo-482#
+```
+---
+
+## Security note
+
+simpass is designed for:
+- Temporary credentials
+- Initial account setup
+- Verbal password delivery
+
+It is not intended for:
+- High-security environments
+- Long-term credential storage
+
+
+---
+
+## Setup Steps
+
+### 1. Open your PowerShell profile
+
+Run this in PowerShell:
+
+```powershell
+notepad $PROFILE
+```
+
+- If the file does not exist, PowerShell will ask to create it click **Yes**.
+
+### 2 Add the `simpass` function
+
+Paste the contents of simpass.ps1 into the profile file
+
+### 3️ Save and close Notepad
+
+### 4️ Allow scripts to run if not already
+
+```powershell
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
+```
+Read more about [Execution Policies](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.security/set-executionpolicy?view=powershell-7.5)
+
+### 5 Close / restart Terminal or PSE
+
+### 6 Test it
+
+```powershell
+simpass
+```
+
+You should see a password printed and copied to the clipboard.
+
+---
+
---
+++ simpass.ps1
@@ -0,0 +1,29 @@
+﻿function simpass {
+    $descriptors = @(
+        "Excited", "Silver", "Rusty", "Battle", "Sunny", "Silent", "Brave", "Happy", "Curious", "Friendly", "Kind", "Cool", "Tricky", "Atomic", "Universal", "Helpful", "Techno", "Electric", "Heavy", "Bright", "Funny", 
+        "Crazy", "Magnetic", "Sleepy", "Electronic", "Automatic", "Swift", "Clever", "Mighty", "Lucky", "Cosmic", "Nimble", "Silent", "Rapid", "Bold", "Neon", "Turbo", "Hyper", "Digital", "Solar", 
+        "Lunar", "Galactic", "Cosmic", "Shadow", "Crimson", "Dynamic", "Epic", "Fearless", "Glorious", "Stealthy", "Witty", "Cheeky", "Playful", "Mechanical", "Synthetic", "Robotic", "Cyber", "Nano", "Ultra",
+        "Relaxed", "Breezy", "Mellow", "Snappy", "Sparkly", "Bouncing", "Quirky", "Goofy", "Jolly", "Cranky", "Impressed", "Super", "Dancing", "Ecstatic", "Grumpy", "Familiar", "Absolute", "Atmospheric", "Interstellar", 
+        "Animated", "Radioactive", "Nuclear", "Manufactured", "Revolving", "Evolving", "Upgraded", "Enhanced", "Energetic", "Chromatic", "Monochromatic", "Original", "Dizzy", "Powered", "Soaring", "Gliding", "Aerodynamic"
+    )
+
+    $things = @(
+        "Crab", "Tree", "Lion", "Kitten", "Coffee", "Pebble", "Coin", "Fridge", "Bear", "Cactus", "Pineapple", "Monkey", "Kangaroo", "Vacuum", "Snake", "Clouds", "Garden", "Dolphin",
+        "Panther", "Mascot", "Elephant", "Stawberry", "Watermelon", "Hoover", "Ladder", "Jungle", "Tarzan", "Turtle", "Helicopter", "Kitchen", "Microwave", "Asteroid", "Blueberry",
+        "Octopus", "Telephone", "Umbrella", "Coconut", "Bicycle", "Calculator", "Mountain", "Volcano", "Rainforest", "Waterfall", "Hurricane", "Crocodile", "Flamingo", "Butterfly", "Ladybug", 
+        "Spaceship", "Astronaut", "Telescope", "Starfish", "Seahorse", "Lunchbox", "Pepper", "Rockmelon", "Magpie", "Saucepan", "Frypan", "Dragon", "Cucumber", "Anteater", "Megatron", "Panda", "Dinosaur", 
+        "Jellyfish", "Hamster", "Cheesecake", "Dragonfly", "Spaghetti", "Railway", "Pancake", "Meatloaf", "Meatball", "Godzilla", "Plankton", "Postman", "Lizard", "Bandicoot", "Gorilla", "Radish", "Pencil",
+        "Stocking", "Trailer", "Jetski", "Fireflies", "Mayonaise", "Cheeseburger", "Burrito", "Cornholio", "Battery", "Stapler", "Bungalow", "Dugong", "Geese", "Goose", "Dingo", "Pelican", "Squirrel", "Hummingbird", "Seaweed", "Shuttlecock",
+        "Albatross"
+    )
+
+    $sep = @("-", ".", "_") | Get-Random
+    $num = Get-Random -Minimum 100 -Maximum 1000
+    $sym = @("!","@","#","$","%","&","*","+","^") | Get-Random
+
+    $password = "$($descriptors | Get-Random)$sep$($things | Get-Random)$sep$num$sym"
+
+    $password
+    $password | Set-Clipboard
+
+}
